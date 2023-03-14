08a381d937898500d6c85ef3bf52e5cef4e9b06f
Hassan Ashfaq <60746056+Hassan-Ashfaq@users.noreply.github.com>
2023-03-14 20:32:50 +0500

    Commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def Train():
+    data = pd.read_csv('Salary_Data.csv')
+    model = LinearRegression()
+    model.fit(
+        data['YearsExperience'].values.reshape(-1, 1), 
+        data['Salary'].values.reshape(-1, 1)
+    )
+    print('Model Trained :-)')
+    return model
+
+model = Train()
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        year = float(request.form['Y101'])
+        out = int(model.predict(np.array(year).reshape(1, -1)))
+        return render_template('predict.html', out=out)
+    else:
+        return render_template("file.html")
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        host='0.0.0.0', 
+        use_reloader=False
+    )
